f60c91435c12703968b7c5acef797c327d883d92
rania <rania@DESKTOP-0T4CALF>
2022-06-17 23:38:35 +0300

    Add project files.

---
+++ ConsoleApp12/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter_9
+{
+   public class point
+    {
+        private int x;
+        private int y;
+        public point() { 
+            
+            x = 0; y = 0; }
+        public point(int xvalue, int yvalue)
+        {
+            x = xvalue;
+            y = yvalue;
+        }
+
+        public int X
+        {
+            set { x = value; }
+            get { return x; }
+        }
+        public int Y
+        {
+            set { y = value; }
+            get { return y; }
+        }
+        public override string ToString()
+        {
+            return "(" + x + "," + y + ")";
+        }
+
+    }
+
+
+
+
+
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            point p1 = new point(); //(0,0)
+        
+            point p2 = new point(72,115); // (72,115)
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p2);
+        }
+    }
+}
